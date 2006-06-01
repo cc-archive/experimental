4e6bacb9a0b586e2baf64bdfc2a6ebebe83aa38a
kidproto <kidproto@committer.creativecommons.org>
2006-06-01 15:13:15 +0000

    Adapted class as a generalized language solution and also got it basically ready to plug into cchost.

--- www/i18ntest/index.php
+++ www/i18ntest/index.php
@@ -1,11 +1,11 @@
 <?php
     include_once('header.inc');
 
-    echo gettext("A string to be translated would go here");
+    echo "<p>", gettext("A string to be translated would go here"), "</p>";
     
-    echo gettext("I like having a creative future.");
+    echo "<p>", gettext("I like having a creative future."), "</p>";
 
-    echo gettext("One more for the books");
+    echo "<p>", gettext("One more for the books"), "</p>";
 
     include_once('footer.inc');
 ?>
